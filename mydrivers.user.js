feaae5c638972982a804df1e87ec70e183a1ccec
forjk <45136955+forjk@users.noreply.github.com>
2018-11-18 14:12:54 +0800

    Update mydrivers.user.js

--- mydrivers.user.js
+++ mydrivers.user.js
@@ -2,7 +2,7 @@
 // @name        驱动之家
 // @namespace   https://github.com/forjk/tampermonkey
 // @include     https://news.mydrivers.com/1/*
-// @version     1.0
+// @version     1.1
 // @require     http://code.jquery.com/jquery-2.1.1.min.js
 // @grant       none
 	
