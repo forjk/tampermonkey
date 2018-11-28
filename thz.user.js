07fb6468a4743788d0a4a5ce829dfc33c39a99b4
forjk <45136955+forjk@users.noreply.github.com>
2018-11-28 21:35:34 +0800

    Update thz.user.js

--- thz.user.js
+++ thz.user.js
@@ -5,11 +5,12 @@
 // @description  try to take over the world!
 // @author       You
 // @match        http://thzbt.co/forum-181-*.html
+// @match        http://thzbt.co/thread-*
 // @require     http://apps.bdimg.com/libs/jquery/1.11.3/jquery.js
 // @grant       GM_xmlhttpRequest
 // @grant       GM_notification
 // @connect     115.com
-// @connect     btso.pw
+// @connect     btsow.pw
 // ==/UserScript==
 
 (function () {
@@ -39,6 +40,89 @@
                 }
             }
         });
-        
+    });
+
+
+    if($("#thread_subject").length == 1){
+        var text = $("#thread_subject").text().substring(11);
+        var key = text.match(reg);
+        if(text.indexOf("fc2ppv") != -1 && key != null ){
+
+            GM_xmlhttpRequest({
+                url: "https://btsow.pw/search/" + key[0],
+                method  : "GET",
+                //responseType : "json",
+                onload: function (data) {
+                    $("ignore_js_op").eq(0).before(key[0] + "<br/>");
+
+                    $(data.responseText ).find(".data-list a").each(function (i) {
+                        var url = $(this).attr("href");
+                        url = url.substring(url.lastIndexOf("/") + 1);
+                        url = "http://115.com/web/lixian/?ct=lixian&ac=add_task_url&url=magnet:?xt=urn:btih:" + url;
+
+                        var title = $(this).attr("title");
+                        var size = $(this).next().html();
+                        var date = $(this).nextAll().last().html();
+
+                        var color = "";
+                        if(title.indexOf(".mp4") >0 || title.indexOf(".wmv") >0 || title.indexOf(".avi") >0){
+                            color = "color:blue;";
+                        }
+
+                        title = title + " [" + date + "]" + " [" + size + "] ";
+                        $("ignore_js_op").eq(0).before('<a class="btsolink" style="' + color +  '" target="_blank" href="' + url + '">' + title + '</a></br>');
+                    });
+
+                    $("ignore_js_op").eq(0).before("<br/><br/>");
+
+
+                    $(".btsolink").on("click",function(){
+                        $(this).css("color","#800080");
+                        var href = $(this).attr("href");
+
+                        GM_xmlhttpRequest({
+                            url: href,
+                            method  : "GET",
+                            responseType : "json",
+                            onload : function(data ){
+                                if(data.response.errcode > 0){
+                                    GM_notification({
+                                        text: data.response.error_msg,
+                                        title: "离线失败！",
+                                        timeout: 3500
+                                    });
+                                }else{
+                                    GM_notification({
+                                        text: "离线成功！",
+                                        title: "离线成功！",
+                                        timeout: 1500});
+                                }
+
+                                //history.pushState({ }, "test",$(this).attr("href"));
+
+                                return false;
+                            }
+                        });
+                        return false;
+                    });
+                }
+            });
+        }
+    }
+
+    $('.attnm a').each(function () {
+        var href = $(this).attr('href');
+        var aid = href.substring(href.lastIndexOf("=") +1);
+        href = "forum.php?mod=attachment&aid=" + aid;
+        $(this).attr('href', href);
+        $(this).attr('onclick', "");
+    });
+
+    $('.plhin').eq(0).find("img[lazyloadthumb]").each( function(){
+        var src = $(this).attr('file');
+        $(this).attr('src', src);
+        $(this).removeAttr('file');
+        $(this).removeAttr('lazyloadthumb');
+        $(this).attr('width', 800);
     });
 })();
