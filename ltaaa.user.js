b5baf7b355815f9754ee6efb0bede71fe779167f
forjk <45136955+forjk@users.noreply.github.com>
2018-12-28 11:32:09 +0800

    Update ltaaa.user.js

--- ltaaa.user.js
+++ ltaaa.user.js
@@ -1,28 +1,40 @@
 // ==UserScript==
 // @name        龙腾网
 // @namespace   https://github.com/forjk/tampermonkey
-// @version     1.0
+// @version     1.0.1
 // @include     http://www.ltaaa.com/bbs/*
 // @include     http://www.ltaaa.com/
+// @include     http://translate.ltaaa.com/*
 // @require     http://code.jquery.com/jquery-2.1.1.min.js
 // @grant       none
 // ==/UserScript==
 
 jQuery.noConflict();
 jQuery(document).ready(function ($) {
-  $('#frameechxKu a').each(function () {
-    var url = $(this).attr('href');
-    var id = url.substring(url.indexOf('-') + 1, url.lastIndexOf('-'));
-    url = 'forum.php?mod=forumdisplay&fid=' + id + '&filter=author&orderby=dateline';
-    $(this).attr('href', url);
-    $(this).attr('target', '_self');
-  });
-  
-  $('.hNav ul li').eq(1).find('a').each(function () {
-    var url = $(this).attr('href');
-    var id = url.substring(url.indexOf('-') + 1, url.lastIndexOf('-'));
-    url = '/bbs/forum.php?mod=forumdisplay&fid=' + id + '&filter=author&orderby=dateline';
-    $(this).attr('href', url);
-    $(this).attr('target', '_self');
-  });
+
+    setTimeout(function(){
+        $("body").removeAttr("oncontextmenu");
+        $("body").removeAttr("ondragstart");
+        $("body").removeAttr("onselectstart");
+        $("body").removeAttr("onselect");
+        $("body").removeAttr("oncopy");
+        $("body").removeAttr("onbeforecopy");
+
+    }, 1000);
+
+    $('#frameechxKu a').each(function () {
+        var url = $(this).attr('href');
+        var id = url.substring(url.indexOf('-') + 1, url.lastIndexOf('-'));
+        url = 'forum.php?mod=forumdisplay&fid=' + id + '&filter=author&orderby=dateline';
+        $(this).attr('href', url);
+        $(this).attr('target', '_self');
+    });
+
+    $('.hNav ul li').eq(1).find('a').each(function () {
+        var url = $(this).attr('href');
+        var id = url.substring(url.indexOf('-') + 1, url.lastIndexOf('-'));
+        url = '/bbs/forum.php?mod=forumdisplay&fid=' + id + '&filter=author&orderby=dateline';
+        $(this).attr('href', url);
+        $(this).attr('target', '_self');
+    });
 });
